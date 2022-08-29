eb6b5711ff2d27769541b977ba0bd4ed87aed147
dosfx <andrew.adbrown@gmail.com>
2022-08-28 21:45:36 -0400

    rejigger that initial commit

--- ActiveLabel.cs
+++ ActiveLabel.cs
@@ -6,9 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SortingAlgorithms
+namespace DarkControls
 {
-    internal class ActiveLabel : Label
+    public class ActiveLabel : Label
     {
         private bool hover;
 
